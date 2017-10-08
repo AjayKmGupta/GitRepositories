dd854eea5d236407cc4a6d962eec50a2ad2d2603
Ajay Gupta <ajayk8353@gmail.com>
2017-10-09 01:48:21 +0530

    Changes related to functional test case.

---
+++ Demo/src/test/java/com/sdet/selenium/EmployeeSeleniumTest.java
@@ -0,0 +1,39 @@
+package com.sdet.selenium;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class EmployeeSeleniumTest {
+	
+	private WebDriver driver;
+	
+	@BeforeSuite
+	public void setupDriver() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void clickOnViewAllButton() {
+		driver.get("http://localhost:8085/Demo/");
+		WebElement el = driver.findElement(By.xpath("//*[contains(text(),'View All')]"));
+		el.click();
+		assertTrue(driver.findElement(By.cssSelector("[class*='text-success']")).isDisplayed());
+	}
+
+	@AfterSuite
+	public void destroyDriver() {
+		driver.close();
+	}
+	
+}
